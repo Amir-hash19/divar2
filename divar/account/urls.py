7b22eb8864cf304a5af3ede0cfebc8e632004d7a
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-27 20:26:31 +0330

    import postman results

--- divar/account/urls.py
+++ divar/account/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import home_view, register_user
+from .views import (home_view, register_user, 
+                    delete_user, update_user,
+                    get_user)
 
 urlpatterns = [
     path("", home_view, name="home-view"),
-    path("create-user", register_user, name="create-user"),
+    path("create-user/", register_user, name="create-user"),
+    path("delete-user/<int:user_id>", delete_user, name="delete-user"),
+    path("update-user/<int:user_id>", update_user, name="update-user"),
+    path("show-user/<int:user_id>", get_user, name="get-user"),
 ]
--- divar/account/views.py
+++ divar/account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import UserAccount
+from django.shortcuts import get_object_or_404
 import json
 
 
@@ -21,3 +22,68 @@ def register_user(request):
             age = data.get("age")
         )
         return HttpResponse(f"{created_user.id} user created successfully")
+
+
+
+@csrf_exempt
+def delete_user(request, user_id):
+    if request.method == "DELETE":
+        user = get_object_or_404(UserAccount, id=user_id)
+        user.delete()
+        return HttpResponse("The user deleted successfully")
+        
+    
+
+@csrf_exempt
+def update_user(request, user_id):
+    try:
+        user = UserAccount.objects.get(id=user_id)
+    except UserAccount.DoesNotExist:
+        return JsonResponse({"error":"User not found"}, status=404)
+    
+    if request.method in ["PUT","PATCH"]:
+        data = json.loads(request.body)
+        if request.method == "PUT":
+            user.email = data.get("email", user.email)
+            user.name = data.get("name", user.name)
+            user.lastname = data.get("lastname", user.lastname)
+            user.age = data.get("age", user.age)
+
+        elif request.method == "PATCH":
+            if "email" in data:
+                user.email = data["email"]
+            if "name" in data:
+                user.name = data["name"]
+            if "lastname" in data:
+                user.lastname = data["lastname"]
+            if "age" in data:
+                user.age = data["age"]        
+        user.save()
+        return HttpResponse("The user updated successfully!")
+              
+
+@csrf_exempt
+def get_user(request, user_id):
+    try:
+        user = UserAccount.objects.get(id=user_id)
+        user_data = {
+            "id":user.id,
+            "email":user.email,
+            "name":user.name,
+            "lastname":user.lastname,
+            "age":user.age
+        }  
+        return JsonResponse(user_data)
+    except UserAccount.DoesNotExist:
+        return JsonResponse({"error":"User not found"}, status=404)
+
+                
+
+
+    
+        
+          
+
+
+    
+
