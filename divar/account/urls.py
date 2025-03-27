b42c0bfe5d1e77fdba60d50b7416b75322f720e0
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-27 17:44:07 +0330

    :construction:

--- divar/account/urls.py
+++ divar/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, register_user
 
 urlpatterns = [
     path("", home_view, name="home-view"),
+    path("create-user", register_user, name="create-user"),
 ]
--- divar/account/views.py
+++ divar/account/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import UserAccount
+import json
 
 
 def home_view(request):
     return HttpResponse("this is home page")
+
+
+
+@csrf_exempt
+def register_user(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        created_user = UserAccount.objects.create(
+            email = data.get("email"),
+            name = data.get("name"),
+            lastname = data.get("lastname"),
+            age = data.get("age")
+        )
+        return HttpResponse(f"{created_user.id} user created successfully")
