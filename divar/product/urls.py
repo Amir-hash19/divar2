e962152c334a9a1229c03fcd194b5cb5be9e021f
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-28 18:43:37 +0330

    :construction:

--- divar/product/urls.py
+++ divar/product/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import HomePageView, create_category
+from .views import (HomePageView, create_category,
+                     create_product, delete_product, 
+                     delete_category, display_product,
+                       display_category, update_product, update_category)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home-view"),
     path("/create-category/", create_category, name="create-category"),
+    path("/create-product", create_product, name="create-product"),
+    path("/delete-product/<int:product_id>", delete_product, name="delete-product"),
+    path("/delete-category/<int:category_id>", delete_category, name="delete-category"),
+    path("/display-product/<int:product_id>", display_product, name="display-product"),
+    path("/display-category/<int:category_id>", display_category, name="display-category"),
+    path("/update-product/<int:product_id>", update_product, name="update-product"),
+    path("/update-category/<int:category_id>", update_category, name="update-category"),
     
 ]
--- divar/product/views.py
+++ divar/product/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Product, Category
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 import json
 
 
@@ -23,3 +24,156 @@ def create_category(request):
         )
         return HttpResponse(f"{created_category.id} Category created successfully")
         
+
+
+
+
+@csrf_exempt
+def create_product(request):
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body)
+            category_id = data.get("category_id")
+            if not category_id:
+                return JsonResponse({"error": "category_id is required"}, status=400)
+
+            try:
+                category = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:  
+                return JsonResponse({"error": "category not found"}, status=404)
+
+            created_product = Product.objects.create(
+                title=data.get("title"),
+                price=data.get("price"),
+                description=data.get("description"),
+                slug=data.get("slug"),
+                quantity=data.get("quantity"),
+                category=category,  
+            )
+
+            return JsonResponse({"message": f"Product with ID {created_product.id} was created successfully!"}, status=201)
+
+        except json.JSONDecodeError:
+            return JsonResponse({"error": "Invalid JSON data"}, status=400)
+
+    return JsonResponse({"error": "Invalid request method"}, status=405)
+
+        
+
+
+@csrf_exempt
+def delete_product(request, product_id):
+    if request.method == "DELETE":
+        product = get_object_or_404(Product, id=product_id)
+        product.delete()
+        return HttpResponse("the product was deleted successfully!")
+
+
+    
+
+@csrf_exempt
+def delete_category(request, category_id):
+    if request.method == "DELETE":
+        category = get_object_or_404(Category, id=category_id)
+        category.delete()
+        return HttpResponse("the category was deleted successfully!")
+
+
+
+
+
+@csrf_exempt
+def display_product(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+        product_data = {
+            "id":product.id,
+            "title": product.title,
+            "price": product.price,
+            "description": product.description,
+            "slug": product.slug,
+            "quantity": product.quantity,
+            "category":product.category.id
+        }
+        return JsonResponse(product_data, safe=True)
+    except Product.DoesNotExist:
+        return JsonResponse({"error":"User not found"})
+
+
+
+@csrf_exempt
+def display_category(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+        category_data = {
+            "title":category.title,
+            "slug":category.slug,
+        }
+        return JsonResponse(category_data, safe=True)
+    except Category.DoesNotExist:
+        return JsonResponse({"error":"Category not found!"})
+
+
+
+
+@csrf_exempt
+def update_product(request, product_id):
+    try:
+        product = Product.objects.get(id=product_id)
+    except Product.DoesNotExist:
+        return JsonResponse({"error":"User not found"}, status=404)
+    
+    if request.method in ["PUT","PATCH"]:
+        data = json.loads(request.body)
+        if request.method == "PUT":
+            product.title = data.get("title", product.title)
+            product.price = data.get("price", product.price)
+            product.description = data.get("description", product.description)
+            product.slug = data.get("slug", product.slug)
+            product.quantity = data.get("quantity", product.quantity)
+            product.category = data.get("category", product.category.id)
+
+        elif request.method == "PATCH":
+            if "title" in data:
+                product.title = data["title"]
+            if "price" in data:
+                product.price = data["price"]
+            if "description" in data:
+                product.description = data["description"]
+            if "slug" in data:
+                product.slug = data["slug"]  
+            if "quantity" in data:
+                product.quantity = data["quantity"]       
+        product.save()
+        return HttpResponse("The product updated successfully!")
+
+
+
+@csrf_exempt
+def update_category(request, category_id):
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return JsonResponse({"error":"Category not found"})
+    if request.method == ["PUT","PATCH"]:
+        data = json.loads(request.body)
+        if request.method == "PUT":
+            category.title == data.get("title",Category.title)
+            Category.slug = data.get("slug", Category.price)
+        elif request.method == "PATCH":
+                if "title" in data:
+                    Category.title = data["title"]
+                if "slug" in data:
+                    Category.slug = data["slug"]
+        category.save()
+        return HttpResponse("The category updated successfully")        
+            
+                
+
+        
+
+    
+
+
+
+    
